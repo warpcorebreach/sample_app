add771cfa871df3d4501fb6d8d3aeedc1160af5a
jcoates5327 <jcoates5327@gmail.com>
2020-05-04 21:00:36 -0400

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = '')
+        base_title = 'RoR Tutorial Sample App'
+        if page_title.empty?
+            return base_title
+        else
+            return "#{page_title} | #{base_title}"
+        end
+    end
 end

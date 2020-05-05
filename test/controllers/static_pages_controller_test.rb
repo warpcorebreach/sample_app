fad9bbefb9ade43a010abd556aa89f84ea072292
jcoates5327 <jcoates5327@gmail.com>
2020-05-04 21:44:43 -0400

    Add image to Home page. Add CSS for Bootstrap

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,13 +8,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "RoR Tutorial Sample App"
+    assert_select "title", "Home | RoR Tutorial Sample App"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "RoR Tutorial Sample App"
+    assert_select "title", "Home | RoR Tutorial Sample App"
   end
 
   test "should get help" do

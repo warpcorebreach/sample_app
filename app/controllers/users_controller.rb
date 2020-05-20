40e9c74f46ee90e644e75a0c095c3d65b7b96cd8
jcoates5327 <jcoates5327@gmail.com>
2020-05-19 22:26:07 -0400

    Add error messages to and catch invalid signups.\nAdd tests for invalid signups and error messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    # bad and insecure:    @user = User.new(params[:user])
+    @user = User.new(user_params)
+
+    if @user.save
+      # handle successful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end

ab9bc2cef461d9721a5246573f243f5e8482bb76
jcoates5327 <jcoates5327@gmail.com>
2020-05-22 01:10:00 -0400

    Add alternate header for logged in users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,17 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Returns currently logged in user, if any
   def current_user
     if session[:user_id]
     # @current_user.nil? ? User.find_by(...) : @current_user
     # @current_user = @current_user || User.find_by(...)
       @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
+
+  # Returns true if user logged in
+  def logged_in
+    !current_user.nil?
   end
 end
